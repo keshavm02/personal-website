bf7321af8c9aafc1c233f2605a336d01e15f9079
Keshav <km02@bu.edu>
2019-10-05 22:21:56 -0400

    cleaned up team projects and side bar

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/blog', (req, res) => res.render('pages/blog.ejs'))
   .get('/books', (req, res) => res.render('pages/books.ejs'))
-  .get('/teamproj', (req, res) => res.render('pages/teamproj.ejs'))
+  // .get('/teamproj', (req, res) => res.render('pages/teamproj.ejs'))
   .get('/contact', (req, res) => res.render('pages/contact.ejs'))
   .get('/resume', (req, res) => res.render('pages/resume.ejs'))
 
